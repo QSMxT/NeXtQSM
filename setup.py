504c08aafc02bafb56e5a252759b3856f7c33039
Ashley Stewart <a.stewart.au@gmail.com>
2024-02-09 14:29:28 +1000

    Added missing __init__ files and update to v1.0.3

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-import os
 from setuptools import setup, find_packages
 
 setup(
     name="nextqsm",
     long_description="NeXtQSM is a complete deep learning pipeline for data-consistent quantitative susceptibility mapping trained with hybrid data",
-    version="1.0.2",
+    version="1.0.3",
     packages=find_packages(),
     package_dir={'nextqsm': 'nextqsm'},
     package_data={
